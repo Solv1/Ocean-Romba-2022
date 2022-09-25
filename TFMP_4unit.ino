a01abb32d3ba484019ecf92c3d3b35c79dc0b75f
Petru Malac <petermalac97@gmail.com>
2022-09-25 13:15:25 -0700

    Update TFMP_4unit.ino

--- TFMP_4unit.ino
+++ TFMP_4unit.ino
@@ -24,7 +24,7 @@ int16_t tfDist4 = 0;  // Distance to object in centimeters
 char distStr[6];      // distance data string
 
 unsigned long previousMillis = 0UL;
-const long interval = 10000UL;
+const long interval = 5000UL;
   
 void commands( uint8_t addr)
 {
@@ -72,11 +72,12 @@ void setup()
     }
     else tfmP1.printReply();
 
+
     // Recover I2C Bus from any data hang.
     // This function includes `Wire.begin()`
     tfmP1.recoverI2CBus();
 //    Wire.begin();            // Initialize two-wire interface
-//    Wire.setClock( 400000);  // Set I2C bus speed to 'Fast' if
+    Wire.setClock( 400000);  // Set I2C bus speed to 'Fast' if
                                // your Arduino supports 400KHz.
 
     // Send lidar addresses to interface with
@@ -91,92 +92,113 @@ void loop()
     unsigned long currentMillis = millis();
     
     tfmP1.getData( tfDist1, 0x10);  // Get a frame of data (left lidar)
-    delay(10);
-    tfmP1.getData( tfDist2, 0x14);  // Get a frame of data (top left lidar)
-    delay(10);
+    delay(30);
+    tfmP1.getData( tfDist2, 0x14);  // Get a frame of data (left front lidar)
+    delay(30);
     tfmP1.getData( tfDist3, 0x18);  // Get a frame of data (right lidar)
-    delay(10);
-    tfmP1.getData( tfDist4, 0x1C);  // Get a frame of data (top right lidar)
-    
-  if ((tfDist2 <= 62 && tfDist4 <= 62) || (tfDist3 <= 62 && tfDist4 <= 62) || (tfDist3 <= 62 && tfDist2 >= 62 && tfDist4 >= 62) || (tfDist4 <= 62))
+    delay(30);
+    tfmP1.getData( tfDist4, 0x1C);  // Get a frame of data (right front lidar)
+   
+  if ((tfDist2 <= 62 && tfDist4 <= 62 && tfDist2 != 0 && tfDist4 != 0) || (tfDist3 <= 62 && tfDist4 <= 62 && tfDist3 != 0 && tfDist4 != 0) || (tfDist3 <= 62 && tfDist2 >= 62 && tfDist4 >= 62 && tfDist3 != 0 && tfDist2 != 0 && tfDist4 != 0) || (tfDist4 <= 62 && tfDist4 != 0))
   {
-      servo.write(servoRight);
-      thruster.writeMicroseconds(1350);
-      if(currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
+    servo.write(servoRight);
+    thruster.writeMicroseconds(1650);
+    if(currentMillis - previousMillis > interval) //wait delay (is interruptable)
     {
       previousMillis = currentMillis;
-      thruster.writeMicroseconds(1500);
+      thruster.writeMicroseconds(1650);
     }
   }
-  else if ((tfDist1 <= 62 && tfDist2 <= 62) || (tfDist1 <= 62 && tfDist2 >= 62 && tfDist4 >= 62) || (tfDist2 <= 62)) 
+  else if ((tfDist1 <= 62 && tfDist2 <= 62 && tfDist1 != 0 && tfDist2 != 0) || (tfDist1 <= 62 && tfDist2 >= 62 && tfDist4 >= 62 && tfDist1 != 0 && tfDist2 != 0 && tfDist4 != 0 ) || (tfDist2 <= 62 && tfDist2 != 0)) 
   {
-      servo.write(servoLeft);
-      thruster.writeMicroseconds(1350);
-      if(currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
+    servo.write(servoLeft);
+    thruster.writeMicroseconds(1650);
+    if(currentMillis - previousMillis > interval) //wait delay (is interruptable)
     {
-        previousMillis = currentMillis;
-        thruster.writeMicroseconds(1500);
+      previousMillis = currentMillis;
+      thruster.writeMicroseconds(1650);
     }
   }
   else
   {
-      servo.write(servoMid);
-      thruster.writeMicroseconds(1350);
-      if(currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
+    printf("Distance1: %d\n",tfDist1);
+    printf("Distance2: %d\n",tfDist2);
+    printf("Distance3: %d\n",tfDist3);
+    printf("Distance4: %d\n",tfDist4);
+    servo.write(servoMid);
+    thruster.writeMicroseconds(1650);
+    if(currentMillis - previousMillis > interval) //wait delay (is interruptable)
     {
-        previousMillis = currentMillis;
-        thruster.writeMicroseconds(1500);
+      previousMillis = currentMillis;
+      thruster.writeMicroseconds(1650);
     }
   }
-  
-  
-  
+}
   
   //SECTION COMMENTED OUT BELOW WAS FOR TESTING PURPOSES,
   //AND MAY STILL BE NEEDED FOR NOW.
-    //if (tfDist1 < 31) 
-    //{
-  //  printf("0x10 (LEFT SENSOR) is less than 31cm\n");
-  //  servo.write(servoLeft);
-  //  thruster.writeMicroseconds(1350);
-  //  if(currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
-  //  {
-  //    previousMillis = currentMillis;
-  //    thruster.writeMicroseconds(1500);
-  //  }
-  //}
-    //if (tfDist2 < 31) 
-    //{
-  //  printf("0x14 (FRONT LEFT SENSOR) is less than 31cm\n");
-  //  servo.write(servoRight);
-  //  thruster.writeMicroseconds(1350);
-  //  if(currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
-  //  {
-  //    previousMillis = currentMillis;
-  //    thruster.writeMicroseconds(1500);
-  //  }
-  //}
-  //
-  //if (tfDist3 < 31) 
-  //{
-  //  printf("0x18 (RIGHT SENSOR) is less than 31cm\n");
-  //  servo.write(servoRight);
-  //  thruster.writeMicroseconds(1350);
-  //  if(currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
-  //  {
-  //    previousMillis = currentMillis;
-  //    thruster.writeMicroseconds(1500);
-  //  }
-  //}
-  //if (tfDist4 < 31) 
-  //{
-  //  printf("0x1C (RIGHT FRONT SENSOR) is less than 31cm");
-  //  servo.write(servoRight);
-  //  thruster.writeMicroseconds(1350);
-  //  if(currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
-  //  {
-  //    previousMillis = currentMillis;
-  //    thruster.writeMicroseconds(1500);
-  //  }
-  //}
+    /*
+    if (tfDist1 <= 31 && tfDist1 != 0) 
+    {
+      printf("0x10 (RIGHT FRONT SENSOR) is less than 31cm\n");
+      printf("Distance1: %d\n",tfDist1);
+      servo.write(servoRight);
+      thruster.writeMicroseconds(1650);
+      if (currentMillis - previousMillis > interval) //wait delay (is interruptable)
+      {
+        previousMillis = currentMillis;
+        thruster.writeMicroseconds(1650);
+      }
+    }
+    else if(tfDist2 <= 31 && tfDist2 != 0)
+    {
+      printf("0x14 (FRONT LEFT SENSOR) is less than 31cm\n");
+      printf("Distance2: %d\n",tfDist2);
+      servo.write(servoLeft);
+      thruster.writeMicroseconds(1650);
+      if (currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
+      {
+        previousMillis = currentMillis;
+        thruster.writeMicroseconds(1650);
+      }
+    }
+    else if(tfDist3 <= 31 && tfDist3 != 0)
+    {
+      printf("0x18 (RIGHT SENSOR) is less than 31cm\n");
+      printf("Distance3: %d\n",tfDist3);
+      servo.write(servoLeft);
+      thruster.writeMicroseconds(1650);
+      if (currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
+      {
+        previousMillis = currentMillis;
+        thruster.writeMicroseconds(1650);
+      }
+    }
+    else if(tfDist4 <= 31 && tfDist4 != 0)
+    {
+      printf("0x1C (RIGHT SENSOR) is less than 31cm\n");
+      printf("Distance4: %d\n",tfDist4);
+      servo.write(servoRight);
+      thruster.writeMicroseconds(1650);
+      if (currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
+      {
+        previousMillis = currentMillis;
+        thruster.writeMicroseconds(1650);
+      }
+    }
+    else
+    {
+      printf("Distance1: %d\n",tfDist1);
+      printf("Distance2: %d\n",tfDist2);
+      printf("Distance3: %d\n",tfDist3);
+      printf("Distance4: %d\n",tfDist4);
+      servo.write(servoMid);
+      thruster.writeMicroseconds(1650);
+      if (currentMillis - previousMillis > interval) //should wait for 10 seconds before turning off thruster.
+      {
+        previousMillis = currentMillis;
+        thruster.writeMicroseconds(1650);
+      }
+    }
 }
+    */
